49027640ff7ff7d4fa2550a897987aee1d2060ab
Valentino Cueto <valentinocueto2005@gmail.com>
2024-07-31 16:40:55 -0300

    Ejercicio 2 resuelto

---
+++ src/Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej2Controller : ControllerBase
+{
+    [HttpGet]
+    public string Get([FromQuery] string name, [FromQuery] string city)
+    {
+        return $"Hola {name} bienvenido a {city}";
+    }
+}
