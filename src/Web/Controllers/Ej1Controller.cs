e66529bd74f7dfb2a25b0d9902297f5854db97cb
JulianPaquez <julianpaquez@gmail.com>
2024-07-31 15:33:55 -0300

    Ejercicio 1 resuelto

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej1Controller : ControllerBase
+{
+  [HttpGet()]
+  public int Get( int num1, int num2, int num3)
+  {
+    int suma = num1 + num2 + num3;
+    return suma;
+  }
+}
