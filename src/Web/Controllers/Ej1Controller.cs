6732cf0018b49d7b4944cb2c749feb7a1ca43215
FrancoBalduini <franco233433@gmail.com>
2024-07-31 15:41:37 -0300

    Ej1 Resuelto

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1Controller : ControllerBase
+    {
+        [HttpGet()]
+        public int Get([FromQuery] int num1, [FromQuery] int num2, [FromQuery] int num3)
+        {
+
+            int result = num1 + num2 + num3;
+            return result;
+        }
+    }
+}
