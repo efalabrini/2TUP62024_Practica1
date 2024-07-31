0c5dafa97167b21f179ed92bb1b6eda0741897c7
Juan Martin Masqueda <j.masqueda@gmail.com>
2024-07-31 15:38:06 -0300

    Resolucion ejercicio 1

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej1Controller : ControllerBase
+{
+    [HttpGet]
+    public int Get(int num1, int num2, int num3){
+
+        return num1 + num2 + num3;
+    }
+
+}
