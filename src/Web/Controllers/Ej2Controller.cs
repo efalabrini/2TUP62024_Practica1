7c800c736de581a00a4e80ab470831fa4591a80e
FrancoBalduini <franco233433@gmail.com>
2024-07-31 15:37:04 -0300

    Ej2 Resuelto

---
+++ src/Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get([FromQuery]string nombre, [FromQuery] string ciudad)
+        {
+            string bienvenida = $"Hola {nombre}, bienvenido a la {ciudad}.";
+            return bienvenida;
+        }
+    }
+}
