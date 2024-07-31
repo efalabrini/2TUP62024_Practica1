200da45172bcbcfaf5660a4470f0b714869061f5
Ren991 <beccrenzo@gmail.com>
2024-07-31 18:14:33 -0300

    resolucion ejercicio 3

---
+++ src/Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ej3Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string nombre, int edad)
+        {
+            string message = $"Te llamas {nombre}, y tienes {edad} años.";
+            return Ok(message);
+        }
+    }
+}
