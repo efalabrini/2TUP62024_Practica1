04c9281faccb6dd1c9a6d429093c29343fc31329
Matias D'Anunzio <matidanunzio@gmail.com>
2024-07-31 16:05:47 -0300

    Ejercicio 2 realizado

---
+++ src/Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2Controller : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string name, string city)
+        {
+            return $"Hola {name} bienvenido a {city} ";
+        }
+    }
+}
