e22305b8e0f7b70315c388b17ae6565a3204f484
Matias D'Anunzio <matidanunzio@gmail.com>
2024-07-31 15:42:15 -0300

    Ejercicio 1 realizado

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1Controller : ControllerBase
+    {
+        [HttpGet]
+        public int Get(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+}
