f44085db39ec29b513961806bf832bb85bbd6b9a
Ren991 <beccrenzo@gmail.com>
2024-07-31 17:54:07 -0300

    resolví ejercicio 1

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ej1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get( int a,  int b, int c)
+        {
+            int sum = a + b + c;
+            return Ok("El resultado es " + sum);
+        }
+    }
+}
