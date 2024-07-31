f84cfeb4f0465ac068fc97b9b8efcb2c8a4786f1
FrancoBalduini <franco233433@gmail.com>
2024-07-31 16:00:57 -0300

    Ej5 Resuelto

---
+++ src/Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string dia)
+        {
+            dia = dia.ToLower().Trim();
+
+            string[] semana = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+            string[] findesemana = ["sabado", "domingo"];
+
+
+            if (semana.Contains(dia))
+            {
+                return "Es un dia de semana";
+            }
+            else if (findesemana.Contains(dia))
+            {
+                return "Es un dia del fin de semana";
+            }
+
+            return "El dia ingresado no existe";
+        }
+    }
+}
