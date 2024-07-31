d5d02df4bdb5466bef0a440937d3963fe5ac62a8
Ren991 <beccrenzo@gmail.com>
2024-07-31 18:07:00 -0300

    resolvi ejercicio 2

---
+++ src/Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ej2Controller : Controller
+    {
+        [HttpGet]
+        public IActionResult Get( string nombre, string ciudad)
+        {
+            string mensaje = $"Hola {nombre}, bienvenido a {ciudad}.";
+            return Ok(mensaje);
+        }
+    }
+}
