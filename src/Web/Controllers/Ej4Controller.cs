c9bf908b87d83fd58c88fa6cf3031187ac9ac94e
OscarEMaciel <emaciel19@gmail.com>
2024-08-04 11:50:58 -0300

    Resolvi ejercicio 4

---
+++ src/Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class Ej4Controller : ControllerBase
+{
+
+    [HttpGet]
+
+    public int get(int uno, int dos)
+    {
+        if (uno > dos)
+        {
+            return uno;
+        }
+        else
+        {
+            return dos;
+
+        }
+        
+    }
+
+}
