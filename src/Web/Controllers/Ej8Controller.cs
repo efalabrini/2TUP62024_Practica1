883fa0cc135b39885aeeafe57243d828cec08547
FrancoBalduini <franco233433@gmail.com>
2024-08-06 14:37:27 -0300

    Ejercicio 8 Resuelto

---
+++ src/Web/Controllers/Ej8Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej8Controller : ControllerBase
+    {
+        [HttpGet()]
+        public int Get()
+        {
+            int num = 1;
+
+            while (num < 100)
+            {
+                num++;
+            }
+
+            return num; 
+        }
+    }
+}
