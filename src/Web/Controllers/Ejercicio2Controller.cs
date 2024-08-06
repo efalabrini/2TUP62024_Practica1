e41398f031cc8007fa6345f5296a9bff68d9e64f
ricardobiselli <ricardobiselli@gmail.com>
2024-08-05 22:51:59 -0300

    Ejercicio 2 resulto

--- src/Web/Controllers/Ejercicio2Controller.cs
+++ src/Web/Controllers/Ejercicio2Controller.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
