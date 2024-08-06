95df55746ae734735559b5a7afcb9785321fcc07
FrancoBalduini <franco233433@gmail.com>
2024-08-06 14:40:35 -0300

    Ejercicio 9 Resuelto

---
+++ src/Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej9Controller : ControllerBase
+    {
+        [HttpGet]
+        public int[] Get()
+        {
+            var numerosPares = new List<int>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    numerosPares.Add(i);
+                }
+            }
+
+            return numerosPares.ToArray(); 
+        }
+    }
+}
