e28a7c5e271f04fc6f6b341acb4de3dd9a567d9e
Sergio Duarte <romanduarte17@gmail.com>
2024-08-07 15:21:02 -0300

    EJ3Resuelto

---
+++ src/Web/Controllers/EJ3Controller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+public class EJ3Controller:ControllerBase
+{
+    [HttpGet]
+    public string Get(string nombre , string edad)
+    {
+
+        return $"te llamas {nombre}, y tienes {edad} años";
+    }
+}
