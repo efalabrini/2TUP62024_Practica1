5a413e5d021f0fefe93a337a72ea9bc129c9e87b
Francisco Palena <palenafrancisco@gmail.com>
2024-08-07 20:54:24 -0300

    Ejercicios 8-9-10 y constructor Lists

---
+++ src/Web/Controllers/ej10controller.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+
+public class ej10controller : ControllerBase
+{
+
+    [HttpGet]
+
+    public IActionResult Get(int start, int end)
+    {
+
+        if (start < 0)
+        {
+            return BadRequest("No se permiten valores negativos en el Inicio!");
+        }
+
+        if (end <= 0)
+        {
+            return BadRequest("No se permiten valores negativos o 0!");
+        }
+
+        int result = end - start;
+
+        if (result <= 1000)
+        {
+            List<int> pairList = new List<int>();
+            List<int> oddList = new List<int>();
+
+            for (int i = start; i < end; i++)
+            {
+                if (i % 2 == 0 && i != 0)
+                {
+                    pairList.Add(i);
+                }
+                if (i % 3 == 0 && i != 0)
+                {
+                    oddList.Add(i);
+                }
+            }
+
+            var listas = new Lists
+            {
+                PairList = pairList.ToArray(),
+                OddList = oddList.ToArray()
+            };
+
+
+            return Ok(listas);
+
+
+        }
+        return BadRequest("Error el intervalo no puede ser mayor a 1000.");
+    }
+}
