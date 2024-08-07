fd5d6df0bf5fe688b088a8626b92275c9aecb037
FrancoBalduini <franco233433@gmail.com>
2024-08-07 14:16:05 -0300

    Ej3 Linq Resuelto

---
+++ src/Web/Controllers/Ej3LinqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<string> palabras)
+        {
+            var respuesta = from string pal in palabras
+                        where pal[0].ToString() == "b" && pal.EndsWith('r')
+                        select pal;
+
+            return respuesta.ToList();
+        }
+
+    }
+}
