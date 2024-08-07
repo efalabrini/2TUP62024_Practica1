bee0015b612820ffd551c77c5bd603c0a8d8efde
MarianoGerosa <gerosamariano@gmail.com>
2024-08-07 13:34:01 -0300

    Ejercicio 4 resuelto

---
+++ src/Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MayorAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] int num1, [FromQuery] int num2)
+        {
+            int mayor = (num1 > num2) ? num1 : num2;
+            return Ok(mayor);
+        }
+    }
+}
