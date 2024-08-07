303866eea74f0574354a39c9c165fdf523b3ed0f
TommyGun1510 <bytomas135@gmail.com>
2024-08-07 14:28:37 -0300

    update

---
+++ src/Web/Controllers/Ej1controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej1Controller : ControllerBase
+{
+    [HttpGet]
+    public int Get(int uno, int dos, int tres)
+    {
+        return uno + dos + tres;
+    }
+}
+
