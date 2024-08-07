81a531ab9fce11f49dd3de9c0e028dcd826836af
Depeex <fdepeex@gmail.com>
2024-08-07 14:36:54 -0300

    Resolucion ej 2

---
+++ src/Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,18 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+
+public class Ej2Controller : ControllerBase {
+
+    [HttpGet]
+
+    public string Get(string nombre, string ciudad) {
+        return $"Hola {nombre}, bienvenido a {ciudad}!";
+    }
+
+}
