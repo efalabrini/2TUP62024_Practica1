176a9c7acb440b3514c4d487016f2504f37dbf89
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-07 13:50:50 -0300

    Ejercicio 4 resuelto

---
+++ src/Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet]
+        public int Get(int num1, int num2)
+        {
+            if(num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+    }
+}
