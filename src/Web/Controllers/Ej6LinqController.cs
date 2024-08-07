6cc51453b7c3a153802bb688d27b6cc341fb3bac
FrancoBalduini <franco233433@gmail.com>
2024-08-07 14:58:45 -0300

    Ej6 Linq Resuelto

---
+++ src/Web/Controllers/Ej6LinqController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej6LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<string> palabras)
+        {
+            var respuesta = from string pal in palabras
+                        select pal.Replace("ia", "*");
+
+            return respuesta.ToList();
+        }
+
+    }
+}
