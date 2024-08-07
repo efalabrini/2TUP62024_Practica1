9d9c8e21b3df334a325299ceaa22cad78fd2e022
FrancoBalduini <franco233433@gmail.com>
2024-08-07 15:17:01 -0300

    Ej7 Linq Resuelto

---
+++ src/Web/Controllers/Ej7LinqController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej7LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public IList<string> Get([FromQuery] string palabras)
+        {
+            return DevolverPalabrasCapitales(palabras);
+        }
+        public static IList<string> DevolverPalabrasCapitales(string cadena)
+        {
+            var cadenaSplit = cadena.Split(" ");
+
+            var respuesta = from pal in cadenaSplit
+                            where pal == (pal.ToUpper())
+                            select pal;
+
+
+            return respuesta.ToList();
+        }
+    }
+}
+}
