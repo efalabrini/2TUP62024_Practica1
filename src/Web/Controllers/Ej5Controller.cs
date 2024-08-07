884f6f7adb7c240911d7d8149796c4c51410fb55
Felipe Gil <felipeegil05@gmail.com>
2024-08-07 16:32:53 -0300

    Ejercicio 5

--- src/Web/Controllers/Ej5Controller.cs
+++ src/Web/Controllers/Ej5Controller.cs
@@ -10,16 +10,13 @@ namespace Web.Controllers
         [HttpGet]
         public string Get(string dia)
         {
-            // Quitar espacios en blanco al inicio y al final y convertir a minúsculas
             string diaNormalizado = dia.Trim();
 
-            // Verificar si el día ingresado es fin de semana
             if (string.Equals(diaNormalizado, "sabado", StringComparison.OrdinalIgnoreCase) ||
                 string.Equals(diaNormalizado, "domingo", StringComparison.OrdinalIgnoreCase))
             {
                 return $"{diaNormalizado} es fin de semana.";
             }
-            // Verificar si el día ingresado es un día laborable
             else if (string.Equals(diaNormalizado, "lunes", StringComparison.OrdinalIgnoreCase) ||
                     string.Equals(diaNormalizado, "martes", StringComparison.OrdinalIgnoreCase) ||
                     string.Equals(diaNormalizado, "miercoles", StringComparison.OrdinalIgnoreCase) ||
@@ -28,7 +25,6 @@ namespace Web.Controllers
             {
                 return $"{diaNormalizado} no es fin de semana.";
             }
-            // Manejar errores en el input
             else
             {
                 return "Error: día ingresado no válido.";
