1b9e866ccef1e64d1c3acd1b8abf61e2a745cfc3
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-07 14:13:03 -0300

    Ejercicio 5 resuelto

---
+++ src/Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string day)
+        {
+            if (day.ToLower() == "sabado" || day.ToLower() == "domingo")
+            {
+                return $"{day} no es un dia de la semana";
+            }
+            else
+            {
+                return $"{day} es un dia de la semana";
+            }
+        }
+    }
+}
