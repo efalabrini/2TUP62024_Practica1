e5b3ac080d4da7bd7c68b716b9087e5d11884ee2
JulianPaquez <julianpaquez@gmail.com>
2024-08-07 13:28:45 -0300

    ejercicio 2 terminado

---
+++ src/Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej2Controllers : ControllerBase{
+    [HttpGet]
+    public string Get(string name, string city){
+        return $"Hola {name}, bienvenido a {city}.";
+    }
+}
