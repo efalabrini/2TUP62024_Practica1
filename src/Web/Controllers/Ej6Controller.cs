a3bc3c637f27fd980861f5089314b46dd1b8286f
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-07 15:05:48 -0300

    Ejercicio 6 resuelto

---
+++ src/Web/Controllers/Ej6Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej6Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(float price, int quantity, string pay, string cardNumber)
+        {
+            if(pay.ToLower() == "tarjeta")
+            {
+                if(cardNumber is null)
+                {
+                    return BadRequest("tarjeta invalida");
+                }
+                if (cardNumber.Length != 16)
+                {
+                    return BadRequest("el numero de la tarjeta debe contener 16 caracteres");
+                }
+                //decimal partialPrice = price
+                return Ok((price * 1.10) * quantity);
+            }
+            else if(pay.ToLower() == "efectivo")
+            {
+                return Ok(price * quantity);
+            }
+            else
+            {
+                return BadRequest("forma de pago no autorizada");
+            }
+        }
+    }
+}
