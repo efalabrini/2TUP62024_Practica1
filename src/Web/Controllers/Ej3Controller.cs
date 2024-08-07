fb35218e647368ec95a6190b7008a1258e6c102d
Mateo Fux <106816941+Mateof311@users.noreply.github.com>
2024-08-07 15:43:01 -0300

    resolucionej3

---
+++ src/Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+
+//Reciba un nombre y un valor de edad y retorne el siguiente mensaje:
+//“Te llamas ” <nombre> ” y tienes ” <años> ” años”
+
+[ApiController]
+[Route("[controller]")]
+
+public class Ej3Controller : ControllerBase
+{
+    [HttpGet]
+
+    public string Get(string nombre, int edad)
+    {
+        return $"Te llamas {nombre} y tienes {edad} años";
+    }
+}
