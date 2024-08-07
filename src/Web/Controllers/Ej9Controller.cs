c59c802fff8d42a335899ca3c792e31cb3597b94
JulianPaquez <julianpaquez@gmail.com>
2024-08-07 14:35:20 -0300

    Ej9 terminado

---
+++ src/Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej9Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(int inicio, int fin)
+        {
+            if (Math.Abs(fin - inicio) >= 1000)
+            {
+                return BadRequest("La diferencia entre inicio y fin debe ser menor a 1000.");
+            }
+
+            List<int> numeros = new List<int>();
+            int i = inicio;
+            while (i <= fin)
+            {
+                if(i % 2 == 0)
+                {
+                    numeros.Add(i);
+                    
+                }
+                i++;
+            }
+           
+
+            return Ok(numeros);
+        }
+    }
+}
