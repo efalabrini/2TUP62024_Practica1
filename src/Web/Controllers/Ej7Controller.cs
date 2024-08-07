d13bdd74d7a076102501a8e2c7dbd2e2e67fb68f
Ren991 <beccrenzo@gmail.com>
2024-08-07 15:22:18 -0300

    resolví el ejercicio 7

---
+++ src/Web/Controllers/Ej7Controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej7Controller : Controller
+    {
+        [HttpGet("generar")]
+        public IActionResult GenerarRango(
+             int inicio,
+             int fin)
+        {
+            if (fin - inicio >= 1000 || fin < inicio)
+            {
+                return BadRequest("La diferencia entre inicio y fin debe ser menor a 1000 y fin debe ser mayor que inicio.");
+            }
+
+            List<int> numeros = [];
+            for (int i = inicio; i <= fin; i++)
+            {
+                numeros.Add(i);
+            }
+
+            return Ok(numeros);
+        }
+    }
+}
