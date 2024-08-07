ea56e887291f897a9ef67d74aec9a8dec33e240b
Sergio Duarte <romanduarte17@gmail.com>
2024-08-07 15:38:01 -0300

    EJ4RESUELTO

---
+++ src/Web/Controllers/EJ4Controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+
+public class EJ4Controller: ControllerBase
+{
+    [HttpGet]
+
+    public int Get(int numero1, int numero2)
+    {
+        if (numero1 > numero2)
+        {
+            return $"{numero1} es mayor que {numero2}";
+        }
+        else if (numero1 < numero2)
+        {
+            return $"{numero2} es mayor que {numero1}";
+        }
+        else
+        {
+            return $"{numero1} es igual a {numero2}";
+        }
+    }
+
+}
