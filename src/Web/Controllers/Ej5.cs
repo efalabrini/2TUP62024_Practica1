9f15eb37ae6d2d3d3b4583f51dc732cb086e1af7
Maximo Martin <marmax0504@gmail.com>
2024-08-08 15:33:34 -0300

    Resolvi Ejercicio 5

---
+++ src/Web/Controllers/Ej5.cs
@@ -0,0 +1,25 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+
+[Route("[controller]")]
+
+public class Ejercicio5 : ControllerBase{
+
+
+    [HttpGet]
+
+    public ActionResult Get(string day)
+     {
+        if (day.ToLower().Trim() == "sabado" || day.ToLower().Trim() =="domingo"){
+            return Ok("Es fin de semana");
+        }else if (day.ToLower().Trim() == "lunes" || day.ToLower().Trim() =="martes" || day.ToLower().Trim() == "miercoles" || day.ToLower().Trim() =="jueves" || day.ToLower().Trim() =="viernes"){
+            return Ok("No es fin de semana");
+        }else {return StatusCode(400);}
+    }
+
+    
+}
