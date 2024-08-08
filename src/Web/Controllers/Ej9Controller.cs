c7c54a6a93240c2851e1c2801230610486b61341
Depeex <fdepeex@gmail.com>
2024-08-08 16:59:43 -0300

    ResolucionEj9

---
+++ src/Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+
+[ApiController]
+[Route("[controller]")]
+
+public class Ej8Controller : ControllerBase {
+    [HttpGet]
+    public ActionResult Get(int a, int b) {
+        if (b - a >= 1000) {
+            return StatusCode(400);
+        } else {
+            var listaNums = new List<int> {a};
+            int i = a+1;
+            while ( i <= b) {
+                listaNums.Add(i);
+                i++;
+            }
+            var listPares = new List<int> {};
+            foreach(int num in listaNums) {
+                if (num % 2 == 0)
+                    listPares.Add(num);
+            }
+            return Ok(listPares);
+        }
+
+    }
+}
