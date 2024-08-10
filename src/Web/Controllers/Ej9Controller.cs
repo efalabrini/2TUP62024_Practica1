996f287e1c96fba7af962c79e09285e2948c0ccc
Francesco Dagostino <francescodagostino1889@gmail.com>
2024-08-09 21:37:35 -0300

    Resuelvo Ej9

---
+++ src/Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej9Controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get(int start, int end)
+    {
+        //Me fijo que el inicio no sea mayor a 1000 o mayor al num final
+        if (start > 1000 || start > end)
+        {
+            return BadRequest("El número de inicio no puede ser mayor a 1000 y tampoco mayor que el número final.");
+        }
+
+        
+        if (end - start >= 1000)
+        {
+            return BadRequest("La diferencia entre el número de inicio y fin debe ser menor a 1000.");
+        }
+        
+        List<int> numbers = new List<int>();
+        int current = start;
+
+        while (current <= end)
+        {
+            if (current % 2 == 0)
+            {
+                numbers.Add(current);
+            }
+            current++;
+        }
+
+        return Ok(numbers);
+    }
+}
