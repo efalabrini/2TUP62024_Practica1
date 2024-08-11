823bf175992d122f693538d5ca569ebccd4be629
Franco Berlochi <francoberlochi@gmail.com>
2024-08-11 17:57:59 -0300

    Eje4 resuelto

---
+++ src/Web/Controllers/Eje4Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej4controller : ControllerBase
+{
+
+    [HttpGet]
+    public int Get(int num1, int num2)
+    {
+        if (num1 > num2)
+        {
+            return num1;  
+        }
+        else
+        {
+            return num2;   //si los dos son iguales da igual cual retorma
+        }
+    }
+}
