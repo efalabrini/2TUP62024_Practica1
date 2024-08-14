57f05fe8d5f8e7c9b1937acb06f5ec3f76a9723e
MarianoGerosa <gerosamariano@gmail.com>
2024-08-14 14:14:59 -0300

    EjercicioLinq 3 resuelto

---
+++ src/Web/Controllers/LinqEj3Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj3Controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<string> palabras)
+    {
+        var resultado = from p in palabras
+                        where p.StartsWith("b") && p.EndsWith("r")
+                        select p;
+
+        resultado.ToList();
+
+        return Ok(resultado);
+    }
+}
