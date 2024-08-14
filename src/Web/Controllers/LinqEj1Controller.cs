ea4d1f48fe518540866048b67d713e376665bab9
MarianoGerosa <gerosamariano@gmail.com>
2024-08-14 14:02:28 -0300

    EjercicioLinq 1 resuelto

---
+++ src/Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj1Controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<int> numeros, [FromQuery] int min, [FromQuery] int max)
+    {
+        var numQuery = from num in numeros
+                       where num >= min && num <= max
+                       select num;
+
+        numQuery.ToList();
+
+        return Ok(numQuery);
+    }
+}
