42b845a3f6e17be9dda53287815697a0135b76fb
MarianoGerosa <gerosamariano@gmail.com>
2024-08-14 14:27:03 -0300

    EjercicioLinq 5 resuelto

---
+++ src/Web/Controllers/LinqEj5Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj5Controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<int> numeros)
+    {
+        var resultado = (from num in numeros
+                         let cuadrado = num * num
+                         where cuadrado > 20
+                         select $"{num} - {cuadrado}").ToList();
+
+        return Ok(resultado);
+    }
+}
