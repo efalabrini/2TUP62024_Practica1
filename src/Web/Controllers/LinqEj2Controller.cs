6f53ee8c38f6d75b6f9fdf9169ae5e4e454d3aa2
MarianoGerosa <gerosamariano@gmail.com>
2024-08-14 14:09:06 -0300

    EjercicioLinq 2 resuelto

---
+++ src/Web/Controllers/LinqEj2Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj2Controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<string> palabras)
+    {
+        var resultado = from p in palabras
+                       where p.Length >= 5
+                       select p.ToUpper();
+
+        resultado.ToList();
+
+        return Ok(resultado);
+    }
+}
