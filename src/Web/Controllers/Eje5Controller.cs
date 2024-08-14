e29ab76d0527dd58a50cad7b8c3632774451c699
Franco Berlochi <francoberlochi@gmail.com>
2024-08-14 14:37:16 -0300

    Eje5 resuelto

---
+++ src/Web/Controllers/Eje5Controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Eje5Controller : ControllerBase
+{
+
+    [HttpGet]
+    public string Get(string dia)
+    {
+        string[] dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+        dia = dia.Trim().ToLower();
+        for (int i = 0; i < dias.Length; i++) {
+            if (dias[i] == dia) {
+                if (i < 5) { 
+                    return "El día que ingresó es un día de semana";
+                }
+                else { return "El día que ingresó es un día de fin de semana"; }
+            }
+                    
+        }
+        return "No se ha encontrado el día";
+    }
+            
+}
+    
