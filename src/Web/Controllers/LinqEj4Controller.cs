74423abb0453da9d538d583cbf9998ab0ef5d41c
MarianoGerosa <gerosamariano@gmail.com>
2024-08-14 14:21:00 -0300

    EjercicioLinq 4 resuelto

---
+++ src/Web/Controllers/LinqEj4Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj4Controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<int> numeros)
+    {
+        var resultado = (from num in numeros
+                        orderby num descending
+                        select num).Take(5).ToList();
+
+        return Ok(resultado);
+    }
+}
