8621eff7f679439af8c6c0b06f98d019ca709618
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-16 11:25:01 -0300

    Ejercicio 4 linq

---
+++ src/Web/Controllers/LinqEj4Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj4Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery]List<int> numbers)
+        {
+            var numQuery = numbers.OrderByDescending(x => x).Take(5).ToList();
+            return Ok(numQuery);
+        }
+    }
+}
