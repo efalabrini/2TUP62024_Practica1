2ecb53b1672f7126d9fac13453d117aedc226e5e
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-16 11:46:06 -0300

    Ejercicio 6 linq

---
+++ src/Web/Controllers/LinqEj6Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj6Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] List<string> words)
+        {
+            var wordsQuery = from word in words
+                             select word.Replace("ia", "*");
+
+            return Ok(wordsQuery);
+        }
+    }
+}
