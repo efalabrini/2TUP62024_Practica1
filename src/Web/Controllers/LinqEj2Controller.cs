ae93ce64b1077e0d05bbf00cf1d0642c62a0ae30
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-16 11:11:34 -0300

    Ejercicio 2 linq

---
+++ src/Web/Controllers/LinqEj2Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj2Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery]List<string> words)
+        {
+            if(words == null || words.Count == 0) return BadRequest("the list cannot be empty");
+            
+            var wordsQuery = words.Where(x => x.Length  >= 5).Select(w => w.ToUpper()).ToList();
+            return Ok(wordsQuery);
+        }
+    }
+}
