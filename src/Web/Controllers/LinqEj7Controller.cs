c87e6fb348ae7cd2b22cee771da5fc094e23504e
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-16 11:53:35 -0300

    Ejercicio 7 linq

---
+++ src/Web/Controllers/LinqEj7Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj7Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string sentence)
+        {
+            var sentenceQuery = sentence.Split(' ').Where(x => x.All(char.IsUpper)).Select(s => s).ToList();
+            return Ok(sentenceQuery);
+        }
+    }
+}
