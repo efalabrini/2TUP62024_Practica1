2738ae1077a082d1a02a84f5059a1df99f8c901c
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-16 11:18:54 -0300

    Ejercicio 3 linq

---
+++ src/Web/Controllers/LinqEj3Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj3Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] List<string> words)
+        {
+            var wordsQuery = words.Where(x => x.ToLower().StartsWith('b') && x.EndsWith('r')).ToList();
+            return Ok(wordsQuery);
+        }
+    }
+}
