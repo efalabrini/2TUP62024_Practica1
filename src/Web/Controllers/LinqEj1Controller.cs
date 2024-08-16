60ebdb3ef14f4eaf727597694351915cb86b6a79
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-16 10:59:00 -0300

    Ejercicio 1 linq

---
+++ src/Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj1Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<int>> Get([FromQuery]List<int> numbers,[FromQuery] int min,[FromQuery] int max)
+        {
+            if(numbers == null || numbers.Count == 0) return BadRequest("the list cannot be empty");
+            
+            var numQuery = numbers.Where(x  => x >= min && x <= max).ToList();
+
+            return Ok(numQuery);
+
+        }
+    }
+}
