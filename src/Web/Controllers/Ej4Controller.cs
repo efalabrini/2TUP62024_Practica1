36c5b6f7d7a2cbb7f2c99c0b125575fefae69f8a
Nicolas Robles <nicolasrobles98@gmail.com>
2024-08-19 17:36:09 -0300

    resolucion ej4

---
+++ src/Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ej4Controller : Controller
+    {
+        [HttpGet]
+        public ActionResult Get(int num1, int num2) {
+            if (num1 == num2)
+            {
+                return Ok("Los numeros son iguales");
+            }
+            else if (num1 < num2) {
+                return Ok(num2);
+            }
+            else {
+                return Ok(num1);
+            }
+        }
+        
+    }
+}
