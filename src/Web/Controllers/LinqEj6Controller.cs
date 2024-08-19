c90fecde7425f8b711f937d585ba5c09ae469c03
Agustin <agustinferraresi5@gmail.com>
2024-08-18 23:23:19 -0300

    LINQ Ej6 resuelto

---
+++ src/Web/Controllers/LinqEj6Controller.cs
@@ -0,0 +1,23 @@
+﻿//6) Escriba una consulta que sustituya la cadena 'ia' por el asterisco (*) en una lista de palabras dada.
+//   Para simplificar escribir todo sin acento. Ejemplo: "estudiar", "caminar", "dia" →  "estud*r", "caminar", "d*"
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj6controller : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult Get([FromQuery]List<string> strings)
+    {
+
+        var s = strings.Select(str => str.Replace("ia", "*"));
+
+        return Ok(s);
+
+    }
+}
