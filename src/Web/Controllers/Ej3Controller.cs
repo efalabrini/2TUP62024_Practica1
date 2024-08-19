1286c08c4db650485bdff857250ca5a621e04038
Nicolas Robles <nicolasrobles98@gmail.com>
2024-08-19 17:16:20 -0300

    Resolucion ej 3

---
+++ src/Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ej3Controller : Controller
+    {
+        public string Get(string name, string age) { 
+            string message = $"Te llamas {name} y tienes {age} años";
+            return message;
+        }
+    }
+}
