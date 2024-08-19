2f206b110a3ef94801c5621c645292e822dba7a2
Nicolas Robles <nicolasrobles98@gmail.com>
2024-08-19 16:12:38 -0300

    Resolucion ej 1

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    
+    public class Ej1controller : Controller
+    {
+        [HttpGet()]
+        public int Get(int num1, int num2, int num3) 
+        { 
+            int resultado = num1 + num2 + num3;
+            return resultado;
+        
+        }
+    }
+}
