47b91b51d87faf5938e5e93817a6606a8cd88f2f
Franco Berlochi <francoberlochi@gmail.com>
2024-08-20 15:31:55 -0300

    Eje6 casi resuelto

--- src/Web/Controllers/Eje6Controller.cs
+++ src/Web/Controllers/Eje6Controller.cs
@@ -1,17 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Web.Controllers;
+
+namespace System.Text.RegularExpressions;
 
 
 [ApiController]
 [Route("[controller]")]
 
+  
   public class Eje6Controller : Controller
    {
-        public ActionResult Payment(float price, int amount, string paymentMethod)
+        public ActionResult Payment(float price, int amount, string paymentMethod, string cardNumber)
         {
-            if (price < 0 && amount < 0) return StatusCode(400, "No se pueden ingresar valores negativos");
-            
+            paymentMethod = paymentMethod.ToLower().Trim();
+            var rgx = new Regex(@"^[0-16]{16}$");
+            if (price < 0 && amount < 0) return BadRequest("No se pueden ingresar valores negativos");
+            if (paymentMethod == "tarjeta")
+            {
+                if (rgx.IsMatch(cardNumber))
+                {
+                var total = price * amount * 1.1;
+                return Ok(total);
+                }
+                else 
+                {
+                BadRequest("Número de tarjeta no válido"); 
+                }
+            }
+            else if(paymentMethod == "efectivo")
+            {
+            var total = price * amount;
+            return Ok(total);
+            }
+            else 
+            {
+            return BadRequest("Método de pago inexistente");
+            }
 
         }
    }
