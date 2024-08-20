ffbaa89e34ae3854fdc29d55d0846190c0e80f67
Franco Berlochi <francoberlochi@gmail.com>
2024-08-20 14:59:52 -0300

    Eje6

---
+++ src/Web/Controllers/Eje6Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+
+  public class Eje6Controller : Controller
+   {
+        public ActionResult Payment(float price, int amount, string paymentMethod)
+        {
+            if (price < 0 && amount < 0) return StatusCode(400, "No se pueden ingresar valores negativos");
+            
+
+        }
+   }
+
