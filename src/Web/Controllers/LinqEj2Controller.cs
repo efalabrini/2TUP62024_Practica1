b242f9fc771c697faff3a047c04b95cb7fd7b6b9
marcostamburi <marcose027@hotmail.com>
2024-08-21 14:21:08 -0300

    Ejercicio 2Linq resuelto

---
+++ src/Web/Controllers/LinqEj2Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj2Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] List<string> words) 
+        { 
+            var wordsQuery = words.Where(word => word.Length >= 5).Select(word => word.ToUpper());
+            var result = wordsQuery.ToList();
+            return Ok(result);
+        }
+    }
+}
