8844507e01ec935ff83f955d1442ae07e41c635b
ricardobiselli <ricardobiselli@gmail.com>
2024-08-21 13:42:17 -0300

    resolucion ej1 LINQ

---
+++ src/Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj1Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get([FromQuery] List<int> numbers, int min, int max)
+        {
+            if (min > max)
+            {
+                return BadRequest("min number should not be higher than max!");
+            }
+
+            var filteredNumbers = numbers.Where(n => n >= min && n <= max).ToList();
+
+            return Ok(filteredNumbers);
+        }
+    }
+}
