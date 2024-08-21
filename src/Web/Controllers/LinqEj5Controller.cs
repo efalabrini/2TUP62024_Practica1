cd888aee9311aeba75dbd7b8d33f2c85b0fb1204
ricardobiselli <ricardobiselli@gmail.com>
2024-08-21 15:10:47 -0300

    resolucion Ej5 LINQ

---
+++ src/Web/Controllers/LinqEj5Controller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj5Controller : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<List<int>> Get([FromQuery] List<int> numbers)
+        {
+            if (numbers == null || numbers.Count == 0)
+            {
+                return BadRequest("Please make sure the list of numbers is not empty");
+            }
+
+            var filteredNumbers = numbers
+             .Select(number => new
+             {
+                 Number = number,
+                 Square = number * number
+             })
+             .Where(result => result.Square > 20)
+             .Select(result => $"{result.Number} - {result.Square}")
+             .ToList();
+
+            return Ok(filteredNumbers);
+
+            //var filteredNumbers = numbers
+            //    .Select(x => x * x)
+            //    .Where(n => n > 20)
+            //    .ToList();
+            //return Ok(filteredNumbers);
+
+
+        }
+    }
+}
