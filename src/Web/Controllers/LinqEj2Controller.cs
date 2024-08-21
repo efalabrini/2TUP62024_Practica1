6bc53c680dc9ed549dccc5c594efcf0f66bc2546
candevazquez <candeevazquez3@gmail.com>
2024-08-21 15:29:44 -0300

    Ejercicio 2 Linq

---
+++ src/Web/Controllers/LinqEj2Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+using System.Linq;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj2Controller : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get([FromQuery] List<string> lista)
+    {
+        var filterList = from word in lista
+                         where word.Length >= 5
+                         select word.ToUpper();
+        return Ok( filterList.ToList());
+        
+    }
+}
+
+//2) Escriba una consulta que devuelva palabras de al menos 5 caracteres y las ponga en mayúsculas.
+//Ejemplo: "computadora", "usb" → "COMPUTADORA"
