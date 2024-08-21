e11b5c1d91deb76a5ed24df02c8683332110768a
marcostamburi <marcose027@hotmail.com>
2024-08-21 15:09:57 -0300

    Ejercicio 5Linq resuelto

---
+++ src/Web/Controllers/LinqEj5Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj5Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] List<int> numbers) { 
+            var numbersQuery = numbers.Where(x => x*x > 20 ).Select(x => x.ToString($"{x} - {x*x}"));
+            var result = numbersQuery.ToList();
+            return Ok(result);
+        }
+    }
+}
