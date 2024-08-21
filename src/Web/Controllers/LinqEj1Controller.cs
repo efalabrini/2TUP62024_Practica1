e9ca006474295473461f51b601ac478faf5bfb45
marcostamburi <marcose027@hotmail.com>
2024-08-21 14:05:30 -0300

    Ejercicio 1Linq resuelto

---
+++ src/Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+
+namespace Web.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] int numMin, [FromQuery] int numMax, [FromQuery] List<int> numbers)
+        {
+            var numberList = numbers.Where(x => x >= numMin && x <= numMax).Select(x => x);
+            var result = numberList.ToList();
+            return Ok(result);
+        }
+    }
+}
