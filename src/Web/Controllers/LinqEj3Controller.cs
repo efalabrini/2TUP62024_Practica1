4158eb5d59b67d0990438c2e2465bae3788ad7e3
marcostamburi <marcose027@hotmail.com>
2024-08-21 14:27:55 -0300

    Ejercicio 3Linq resuelto

---
+++ src/Web/Controllers/LinqEj3Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj3Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] List<string> words)
+        {
+            var wordsQuery = from w in words
+                             where w.StartsWith("b") && w.EndsWith("r")
+                             select w;
+            var result = wordsQuery.ToList();
+            return Ok(result);
+        }
+    }
+}
