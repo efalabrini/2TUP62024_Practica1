9ae94bed37ad27f5c102b966194abd897d0edf07
Juan Martin Masqueda <j.masqueda@gmail.com>
2024-08-21 13:53:34 -0300

    Resuelto Ej3Linq

---
+++ src/Web/Controllers/EjLinq3.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class EjLinq2 : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get([FromQuery] List<string> words)
+    {
+
+        var wordQuery = from word in words
+                        where (word[0] == 'b' && word[word.Length - 1] == 'r')
+                        select word;
+
+       
+        return Ok(wordQuery.ToList());
+    }
+
+}
