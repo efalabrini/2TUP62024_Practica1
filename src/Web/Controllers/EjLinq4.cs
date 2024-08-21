0f9e316960451c7795d8fbc5b1d64f3a1e4c8aa5
Juan Martin Masqueda <j.masqueda@gmail.com>
2024-08-21 14:02:59 -0300

    Resuelto Ej4Linq

---
+++ src/Web/Controllers/EjLinq4.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class EjLinq4 : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get([FromQuery] List<int> numbers)
+    {
+
+        var numQuery = (from number in numbers
+                        orderby number descending
+                        select number).Take(5);
+
+
+        return Ok(numQuery);
+    }
+
+}
