c8c9b8e8acb4384dd387a2d9763fd81a0c6627b2
Nicolas Robles <nicolasrobles98@gmail.com>
2024-08-21 15:27:27 -0300

    Resolucion linq3

---
+++ src/Web/Controllers/Linq3Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Linq3Controller : Controller
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery]List<string> words) {
+            
+            var listQuery = words.Where(x => x.StartsWith("b") && x.EndsWith("r")).Select(x => x);
+            var result = listQuery.ToList();
+            return Ok(result);
+        }
+
+    }
+}
