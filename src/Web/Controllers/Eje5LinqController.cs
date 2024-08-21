d8bf5a11250427b3d7fe7f94485fff6f07688be7
Franco Berlochi <francoberlochi@gmail.com>
2024-08-21 15:21:17 -0300

    e

---
+++ src/Web/Controllers/Eje5LinqController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class Eje5LinqController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
