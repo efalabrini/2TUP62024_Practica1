d6c8c412437cfa8d53162d61ff0dc134ca269428
Franco Berlochi <francoberlochi@gmail.com>
2024-08-21 15:07:15 -0300

    Eje3Linq

---
+++ src/Web/Controllers/Eje3LinqController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Eje3Linqcontroller : ControllerBase
+{
+
+    [HttpGet]
+    public ActionResult get([FromQuery] List<string> words)
+    {
+        var result = from word in words
+                     where word.StartsWith("b", StringComparison.OrdinalIgnoreCase) && word.EndsWith("r", StringComparison.OrdinalIgnoreCase)
+                     select word;
+
+        return Ok(result);
+
+    }
+}
