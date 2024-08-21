f6a9ee6085ca6a1d17570d106752ba80257f227a
Juan Martin Masqueda <j.masqueda@gmail.com>
2024-08-21 14:09:21 -0300

    Resuelto Ej5Linq

---
+++ src/Web/Controllers/EjLinq5.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class EjLinq5 : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get([FromQuery] List<int> numbers)
+    {
+
+        var numQuery = from number in numbers
+                        where number*number >= 20
+                        select $"{number} - {number*number}";
+
+
+        return Ok(numQuery);
+    }
+
+}
