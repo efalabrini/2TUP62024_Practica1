c23e67efd8aab6adb4fa70130ce91095cdd19898
Franco Berlochi <francoberlochi@gmail.com>
2024-08-21 16:31:50 -0300

    Eje7Linq

---
+++ src/Web/Controllers/Eje7LinqController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEje7controller : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get([FromQuery] string word)
+    {
+        string[] words = word.Split(' ');
+        var result = words.Where(x => x == x.ToUpper()).Select(x => x).ToList();
+        return Ok(result);
+    }
+}
