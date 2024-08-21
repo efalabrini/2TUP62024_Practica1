886c879749917f0a69aad95a14b6827a8d527967
Juan Martin Masqueda <j.masqueda@gmail.com>
2024-08-21 13:44:55 -0300

    Resuelto Ej2Linq

---
+++ src/Web/Controllers/EjLinq2.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class EjLinq2 : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get([FromQuery] List<string> words)
+    {
+      
+        var wordQuery = from word in words
+                       where (word.Length >= 5)
+                       select word.ToUpper();
+
+
+        return Ok(wordQuery.ToList());
+    }
+
+}
