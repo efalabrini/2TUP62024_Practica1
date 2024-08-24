54e52634c3d9f73cc01087616492c90b315674de
Mateo Fux <106816941+Mateof311@users.noreply.github.com>
2024-08-24 19:35:26 -0300

    Ej1Linq

---
+++ src/Web/Controllers/Ej1linqController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej1linqController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get([FromQuery] List<int> list, int min, int max)
+    {
+        var numberList = list.Where(x => x>=min && x<=max).ToList();
+        return Ok(numberList);
+    }
+}
