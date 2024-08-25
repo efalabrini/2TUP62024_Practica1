778605fb82c7bead6fe6c78511b4c6dfe7c0ab65
Mateo Fux <106816941+Mateof311@users.noreply.github.com>
2024-08-25 03:16:27 -0300

    ejercicio6linq

---
+++ src/Web/Controllers/Ej6linqController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+
+
+public class Ej6linqController : ControllerBase {
+    [HttpGet]
+    public ActionResult Get([FromQuery]List<string> words) {
+        
+        var iaWords = words.Select(x => x.Replace("ia","*")).ToList();
+
+        return Ok(iaWords);
+    }
+}
