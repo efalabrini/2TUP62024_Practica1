980ba1686bb9ab691fa999263deff6fa092c4ae9
Mateo Fux <106816941+Mateof311@users.noreply.github.com>
2024-08-25 03:04:37 -0300

    ejercicio5linq

---
+++ src/Web/Controllers/Ej5linqController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+
+
+public class Ej5linqController : ControllerBase {
+    [HttpGet]
+    public ActionResult Get([FromQuery]List<int> numbers) {
+        
+        var numbersSqr = new List<string>();
+
+        foreach (int n in numbers){
+            int square = n*n;
+            if (square > 20){
+                numbersSqr.Add($"{n} - {square}");
+            }
+        }
+        return Ok(numbersSqr);
+    }
+}
