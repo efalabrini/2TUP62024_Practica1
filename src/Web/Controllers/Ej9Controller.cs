b0032a49e7bd6a0c9567df4ea43e870adc29d15e
Felipe Gil <felipeegil05@gmail.com>
2024-08-27 10:57:33 -0300

    Ejercicio9

---
+++ src/Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej9Controller : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get(int numeroI, int numeroF)
+    {
+        if (Math.Abs(numeroF - numeroI) >= 1000)
+        {
+            return BadRequest("La diferencia entre los numeros de inicio y fin debe ser menor a 1000");
+        }
+
+        List<int> numeros = GenerarLista(numeroI, numeroF);
+
+        return Ok(numeros);
+    }
+
+    private List<int> GenerarLista(int inicio, int fin)
+    {
+        List<int> lista = new List<int>();
+
+        for (int i = inicio; i <= fin; i++)
+        {
+            if (i % 2 == 0)
+            {
+                lista.Add(i);
+            }
+        }
+
+        return lista;
+    }
+}
