13ee0f54b5af37cc8926683f4d47d0215d7f3310
Sebas-ale6 <100642556+Sebas-ale6@users.noreply.github.com>
2024-08-28 14:01:38 -0300

    Ej2LinqResuelto

---
+++ src/Web/Controllers/Ej2LinqController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+
+public class Ej2LinqController : ControllerBase {
+    [HttpGet]
+    public ActionResult Get([FromQuery]List<string> words) {
+        var upperLongWords = words
+            .Where(word => word.Length>=5)
+            .Select(word =>word.ToUpper())
+            .ToList();
+        return Ok(upperLongWords);
+    }
+}
