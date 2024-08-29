3658945709dc6863f741969c6ddd48d834b7009c
Danilo Mercado <mercadodanilo213@hotmail.com>
2024-08-28 23:35:59 -0300

    LinqEj3

---
+++ src/Web/Controllers/LinqEj3Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class LinqEj3Controller : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public ActionResult Get()
+        {
+            List<string> words = new List<string> { "ventilado", "reloj", "buscador", "brabor"};
+            var Query = from word in words
+                        where word.StartsWith("b") && word.EndsWith("r")
+                        select word.ToString();
+            return Ok(Query);
+        }
+    }
+}
+
+
+/*
+3) Escriba una consulta que devuelva las palabras que empiezan por la letra "b" y terminan con la letra "r".
+Ejemplo: "ventilador", "reloj", "buscador" → "buscador"
+*/
