5ae94bbda328d6f3b888631f59cf84d20a193062
Francesco Dagostino <francescodagostino1889@gmail.com>
2024-08-28 23:33:48 -0300

    Resuelvo Ej6Linq

---
+++ src/Web/Controllers/LinqEj6Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj6Controller : ControllerBase
+{
+
+    [HttpGet(Name = "action")]
+    public ActionResult Get([FromQuery] string[] words)
+    {
+        var Query = words.Select(word => word.Replace("ia", "*"));
+
+        return Ok(Query);   
+    }
+}
+
+/*
+ 6) Escriba una consulta que sustituya la cadena 'ia' por el asterisco (*) en una lista de palabras dada. Para simplificar escribir todo sin acento.
+Ejemplo: "estudiar", "caminar", "dia" →  "estud*r", "caminar", "d*"
+
+*/
