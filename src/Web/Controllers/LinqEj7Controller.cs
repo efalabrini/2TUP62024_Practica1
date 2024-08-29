ba0594382de0f14a806cf3a037394646b1b1bdff
Francesco Dagostino <francescodagostino1889@gmail.com>
2024-08-29 00:49:14 -0300

    Cambios en el LinqEj7

--- src/Web/Controllers/LinqEj7Controller.cs
+++ src/Web/Controllers/LinqEj7Controller.cs
@@ -1,33 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 
-namespace Web.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class LinqEj7Controller : ControllerBase
+namespace Web.Controllers
 {
-
-    [HttpGet(Name = "action")]
-    public ActionResult Get()
+    [ApiController]
+    [Route("[controller]")]
+    public class LinqEj7Controller : ControllerBase
     {
-        List<string> words = new List<string> { "Estudiar", "ESTUDIAR", "HoLa", "HOLA"};
-        var query = 
-            from word in words
-            where word == word.ToUpper()
-            select word;
+        [HttpGet(Name = "action")]
+        public ActionResult Get()
+        {
+            List<string> words = "Estudiar ESTUDIAR HoLa HOLA".Split(' ').ToList();
 
+            var query =
+                from word in words
+                where word == word.ToUpper()
+                select word;
 
-        return Ok(query);
+            return Ok(query);
+        }
     }
-}
 
-/*
- *7) Implementar la función 
-
-public static IList<string> DevolverPalabrasCapitales(string cadena)
-
-que devuelva sólo las palabras en mayúsculas de la cadena.
-Ejemplo: "ESTO es un EJEMPLO del EJERcicio" → ESTO, EJEMPLO
-
+/* 
+ * 7) Implementar la función public static IList<string> DevolverPalabrasCapitales(string cadena)
+     que devuelva sólo las palabras en mayúsculas de la cadena.
+     Ejemplo: "ESTO es un EJEMPLO del EJERcicio" → ESTO, EJEMPLO
  */
+}
