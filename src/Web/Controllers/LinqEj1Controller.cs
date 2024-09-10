1b74df7c5c2401a23d388d8024fb2a5f93b5fb2c
Maximo Martin <marmax0504@gmail.com>
2024-09-10 10:29:53 -0300

    Resolucion linq 1

---
+++ src/Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj1Controller : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get(int min, int max)
+    {
+        var listNums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        return Ok(listNums.Where(x => x >= min && x <= max).ToList());
+    }
+}
