0595837f4295df77e62e2ac6314088c2cd53df0a
Maximo Martin <marmax0504@gmail.com>
2024-09-10 10:42:22 -0300

    Reolucion 3 Linq

---
+++ src/Web/Controllers/LinqEj3Controller.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web;
+
+[ApiController]
+[Route("[controller]")]
+public class LinqEj3Controller : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get([FromQuery] List<string> words)
+    {
+        var filteredWords = words.Where(word => word.StartsWith("b") && word.EndsWith("r")).Select(word => word);
+        var result = filteredWords.ToList();
+
+        return Ok(result);
+    }
+}
