479e809586ac6080bf5b211684e63d2245c49f91
¨FranciscoCarballo01¨ <francisco.carballo.fc@gmail.com>
2024-09-22 21:07:03 -0300

    Ej-Linq

---
+++ src/Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+
+public class LinqEj1Controller : ControllerBase
+{
+    [HttpGet]
+
+    public ActionResult Get([FromQuery] List<int> list, int min, int max)
+    {
+        var numberList = (from x in list
+                          where x >= min && x <= max
+                          select x
+                          )
+                          .ToList();
+        return Ok(numberList);
+    }
+
+
+}
