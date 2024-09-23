87019a512716bb52b99dd807018139453b5f01c4
¨FranciscoCarballo01¨ <francisco.carballo.fc@gmail.com>
2024-09-23 17:52:59 -0300

    Ejc6-Linq

---
+++ src/Web/Controllers/LinqEj6Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+    public class LinqEj6Controller : ControllerBase
+    {
+        [HttpGet]
+
+        public ActionResult Get([FromQuery] List<string> wordsIa)
+        {
+            var words =
+                from word in wordsIa
+                where word.Contains("ia")
+                select word.Replace("ia","*");
+            return Ok(words);
+        }
+    }
+
