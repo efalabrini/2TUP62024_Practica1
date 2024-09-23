2f3cf1f1449dc2f1f826e66c49bfa2f50fc3fde2
¨FranciscoCarballo01¨ <francisco.carballo.fc@gmail.com>
2024-09-22 21:47:49 -0300

    Ejc2-Linq

---
+++ src/Web/Controllers/LinqEj2Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+    public class LinqEj2Controller : ControllerBase 
+    {
+    [HttpGet]    
+
+        public ActionResult Get([FromQuery] List<string> wordList) 
+        { 
+            var wordQuery = 
+                from word in wordList
+                where (word.Length >= 5)
+                select word.ToUpper();
+            
+        return Ok(wordQuery);
+        }
+    
+    }
+
