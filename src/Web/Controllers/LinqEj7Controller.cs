d86d14d4be29ff9a37b580827ce7db41f39a4d5b
¨FranciscoCarballo01¨ <francisco.carballo.fc@gmail.com>
2024-09-23 18:35:46 -0300

    Linq-Ejc7

---
+++ src/Web/Controllers/LinqEj7Controller.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class LinqEj7Controller : ControllerBase
+{
+    [HttpGet]
+
+    public ActionResult Get([FromQuery] List<string> wordsList)
+    {
+        var words =
+            from word in wordsList
+            where word == word.ToUpper()
+            select word;
+
+        return Ok(words.ToList());
+    }
+}
