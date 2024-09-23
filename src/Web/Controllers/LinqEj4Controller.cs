f1b0c9b7bbd0bc5974bfe31d83a57ca86794ddac
¨FranciscoCarballo01¨ <francisco.carballo.fc@gmail.com>
2024-09-23 00:12:36 -0300

    Ejc4-Linq

---
+++ src/Web/Controllers/LinqEj4Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+
+    public class LinqEj4Controller : ControllerBase
+    {
+        [HttpGet]
+
+
+        public ActionResult Get([FromQuery] List<int> listNumbers)
+        {
+            var numQuery =
+                (from num in listNumbers
+                orderby num descending
+                select num).Take(5);
+
+            return Ok(numQuery);
+        }
+
+    }
+
