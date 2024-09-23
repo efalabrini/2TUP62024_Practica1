ed24cc022d45b2ca290d4eb6446c2eee119bc0c7
¨FranciscoCarballo01¨ <francisco.carballo.fc@gmail.com>
2024-09-23 00:48:22 -0300

    Ejc5-Linq

---
+++ src/Web/Controllers/LinqEj5Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+    public class LinqEj5Controller: ControllerBase
+    {
+        [HttpGet]
+
+
+        public ActionResult Get([FromQuery] List<int> powList) 
+        {
+            var numQuery = powList.Where(num => Math.Pow(num, 2) > 20).Select(num => $"{num} - {Math.Pow(num, 2)}");
+            return Ok(numQuery);
+        }
+    }
+
